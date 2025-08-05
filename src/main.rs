e0feddb7e3eef41612f5f35d4da4d54ed133522d
hr567 <hr567@hr567.me>
2025-08-05 08:13:38 +0000

    Update code and dependencies
    Signed-off-by: hr567 <hr567@hr567.me>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use std::sync::Arc;
+use std::sync::{Arc, LazyLock};
 
-use once_cell::sync::Lazy;
 use reqwest::Client;
 use scraper::{Html, Selector};
 use tokio::sync::{mpsc, Semaphore};
@@ -8,8 +7,8 @@ use url::Url;
 
 const MAX_TASK: usize = 8;
 
-static URL: Lazy<Url> = Lazy::new(|| Url::parse("https://quotes.toscrape.com/").unwrap());
-static CLIENT: Lazy<Client> = Lazy::new(|| {
+static URL: LazyLock<Url> = LazyLock::new(|| Url::parse("https://quotes.toscrape.com/").unwrap());
+static CLIENT: LazyLock<Client> = LazyLock::new(|| {
     use reqwest::header::{HeaderMap, HeaderValue, USER_AGENT};
     let mut headers = HeaderMap::new();
     let user_agent = HeaderValue::from_static(
@@ -28,18 +27,19 @@ struct Quote {
 }
 
 async fn download_quote_html(idx: usize) -> reqwest::Result<String> {
-    let page_url = URL.join(&format!("page/{}/", idx)).unwrap();
+    let page_url = URL.join(&format!("page/{idx}/")).unwrap();
     let res = CLIENT.get(page_url).send().await?;
     let html = res.text().await?;
     Ok(html)
 }
 
-static QUOTE: Lazy<Selector> = Lazy::new(|| Selector::parse(r#".quote"#).unwrap());
-static TEXT: Lazy<Selector> = Lazy::new(|| Selector::parse(r#".text"#).unwrap());
-static AUTHOR: Lazy<Selector> = Lazy::new(|| Selector::parse(r#".author"#).unwrap());
-static TAG: Lazy<Selector> = Lazy::new(|| Selector::parse(r#".tag"#).unwrap());
-fn parse_quote_html(page: Html) -> Vec<Quote> {
-    page.select(&QUOTE)
+static QUOTE: LazyLock<Selector> = LazyLock::new(|| Selector::parse(r#".quote"#).unwrap());
+static TEXT: LazyLock<Selector> = LazyLock::new(|| Selector::parse(r#".text"#).unwrap());
+static AUTHOR: LazyLock<Selector> = LazyLock::new(|| Selector::parse(r#".author"#).unwrap());
+static TAG: LazyLock<Selector> = LazyLock::new(|| Selector::parse(r#".tag"#).unwrap());
+fn parse_quote_html(html: &str) -> Vec<Quote> {
+    Html::parse_document(html)
+        .select(&QUOTE)
         .map(|quote| Quote {
             text: quote.select(&TEXT).next().unwrap().inner_html(),
             author: quote.select(&AUTHOR).next().unwrap().inner_html(),
@@ -51,24 +51,22 @@ fn parse_quote_html(page: Html) -> Vec<Quote> {
 #[tokio::main]
 async fn main() {
     let pool = Arc::new(Semaphore::new(MAX_TASK));
-    let (tx, mut rx) = mpsc::unbounded_channel::<Quote>();
+    let (tx, mut rx) = mpsc::channel(MAX_TASK);
 
-    for page in 1..20 {
+    for page in 1..12 {
         let pool = Arc::clone(&pool);
         let tx = tx.clone();
         tokio::spawn(async move {
             let _permit = pool.acquire().await.unwrap();
             let text = download_quote_html(page).await.unwrap();
-            let html = Html::parse_document(&text);
-            let quotes = parse_quote_html(html);
-            for quote in quotes.into_iter() {
-                tx.send(quote).unwrap();
-            }
+            tx.send(text).await.unwrap();
         });
     }
     drop(tx);
 
-    while let Some(quote) = rx.recv().await {
-        println!("{:?}", quote);
+    let mut quotes = Vec::new();
+    while let Some(html) = rx.recv().await {
+        quotes.extend(parse_quote_html(&html));
     }
+    println!("{quotes:?}");
 }
